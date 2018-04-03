0f758046eae1d53d8ab90528167c3b0fd3072063
Derek Hubbard <derek.e.hubbard@disney.com>
2018-04-03 12:30:06 -0700

    Refactors Rules Engine Priority Tests
    Extracts common objects used in `json-rules-engine` priority test.

--- javascript/json-rules-engine/test/rulesEnginePriority.js
+++ javascript/json-rules-engine/test/rulesEnginePriority.js
@@ -5,40 +5,26 @@ const expect = chai.expect;
 const Engine = jsonRulesEngine.Engine;
 
 describe('json-rules-engine priority', async () => {
+    const ruleTemplate = {
+        conditions: {
+            any: [
+                {
+                    fact: 'myProperty',
+                    operator: 'equal',
+                    value: 'expectedValue'
+                }
+            ]
+        },
+        event: {
+            type: 'eventType'
+        }
+    };
+
     it('returns events sorted in priority order', async () => {
         // Arrange
-        const highPriorityRule = {
-            conditions: {
-                any: [
-                    {
-                        fact: 'myProperty',
-                        operator: 'equal',
-                        value: 'expectedValue'
-                    }
-                ]
-            },
-            event: {
-                type: 'highPriorityEvent'
-            },
-            priority: 100
-        };
-
-        const lowPriorityRule = {
-            conditions: {
-                any: [
-                    {
-                        fact: 'myProperty',
-                        operator: 'equal',
-                        value: 'expectedValue'
-                    }
-                ]
-            },
-            event: {
-                type: 'lowPriorityEvent'
-            },
-            priority: 10
-        }
-        const rules = [lowPriorityRule, highPriorityRule];
+        const rule1 = {...ruleTemplate, ...{ priority: 100, event: { type: 'highPriorityEvent' } }};
+        const rule2 = {...ruleTemplate, ...{ priority: 10, event: { type: 'lowPriorityEvent' } }};
+        const rules = [rule2, rule1];
         const rulesEngine = new Engine(rules);
         const fact = {
             myProperty: 'expectedValue'
